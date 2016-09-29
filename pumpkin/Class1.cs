001c82c9c10f78e71e351aa601239e846fe38c58
Raymond Justice <Raymond Justice>
2016-09-29 16:47:08 -0600

    Ray code

---
+++ pumpkin/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pumpkin
+{
+    class Class1
+    {
+        string[] cravings = new string[10];
+        string cat = "";
+        string wolf = "";
+        string snake = "";
+        string witch = "";
+        string ghost = "";
+        string skelley = "";
+        string grim = "";
+        string clown = "";
+        string crow = "";
+        string werewolf = "";
+
+        string[] names = new string[10] {"Cat","Wolf","Snake","Witch","Ghost",
+            "Skeleton","Grim Reaper","Scary Clown","Scarecrow","WereWolf"};
+
+        
+    }
+}
